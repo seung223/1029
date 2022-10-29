c7b3ff23eeccc67692abf1bd080f37447e2643ee
chosun <wkdtmdgus0@likelion.org>
2022-10-29 10:00:11 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var input_txt = "글자를 입력하세요";
+  TextEditingController textEditingController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar : AppBar(
+        title : Text('프로젝트')),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller : textEditingController,
+                decoration: InputDecoration(
+                  border : OutlineInputBorder(), labelText: "글자를 입력하세요"),
+              onChanged: (text){
+                  setState(() {
+                    input_txt = text;
+                  });
+              }
+              )
+              ),
+    TextButton(
+      onPressed: (){
+        print(textEditingController.text);
+      },
+      child: Text('텍스트 출력하기')),
+          ],
+        )
+      );
+    //);
+  }
+}
