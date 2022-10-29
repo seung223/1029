1385e813843e0e41c4780a9d9dce3753c8c30789
chosun <wkdtmdgus0@likelion.org>
2022-10-29 10:26:19 +0900

    check box switch

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -41,34 +32,31 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   var input_txt = "글자를 입력하세요";
   TextEditingController textEditingController = new TextEditingController();
+  var val = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar : AppBar(
         title : Text('프로젝트')),
         body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller : textEditingController,
-                decoration: InputDecoration(
-                  border : OutlineInputBorder(), labelText: "글자를 입력하세요"),
-              onChanged: (text){
-                  setState(() {
-                    input_txt = text;
-                  });
-              }
-              )
-              ),
-    TextButton(
-      onPressed: (){
-        print(textEditingController.text);
-      },
-      child: Text('텍스트 출력하기')),
-          ],
-        )
-      );
-    //);
+          children : [
+            Checkbox(
+            value: val,
+            onChanged:(value){
+              setState(() {
+                val = value!;
+              });
+            }
+            ),
+            Switch(
+            value: val,
+            onChanged:(value){
+              setState(() {
+                val = value!;
+              });
+            }),
+    ],
+    ));
+
   }
 }
